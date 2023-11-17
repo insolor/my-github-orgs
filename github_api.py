a6be17d5ac0d77b7c37c85ce240fd5f0efbbbc99
insolor <insolor@gmail.com>
2023-11-17 23:41:56 +0300

    Use st.secrets to get token, fix pydantic warning

--- github_api.py
+++ github_api.py
@@ -2,10 +2,14 @@ from pathlib import Path
 
 import requests
 
+import streamlit as st
+
 from models import ResponseModel
 
 
-def get_data(api_token: str) -> ResponseModel:
+def get_data() -> ResponseModel:
+    api_token = st.secrets["GITHUB_TOKEN"]
+    
     current_dir = Path(__file__).parent
 
     with open(current_dir / "get_data.graphql") as file:
@@ -23,16 +27,14 @@ def get_data(api_token: str) -> ResponseModel:
 
     r = requests.post(url=url, json=json, headers=headers)
     r.raise_for_status()
-    return ResponseModel.parse_obj(r.json())
+    return ResponseModel.model_validate(r.json())
 
 
 if __name__ == "__main__":
     # Only for testing in dev environment
     import json
 
-    from decouple import config
     from rich import print
     
-    token = config("GITHUB_TOKEN")
-    data = get_data(token)
+    data = get_data()
     print(data)
