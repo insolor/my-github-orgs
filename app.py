1ab6e3ea1bac0a3dc9e9247eb27b376c9e87ee0a
insolor <insolor@gmail.com>
2023-11-18 12:08:37 +0300

    Show descriptions in tooltips

--- app.py
+++ app.py
@@ -8,21 +8,20 @@ import github_api
 with st.spinner("Getting data..."):
     user_data = github_api.get_data("insolor")
 
-
 markdown = io.StringIO()
 
 for org in user_data.organizations.nodes:
-    print(f"""## <img src="{org.avatarUrl}" width=24> [{org.name}]({org.url})""", file=markdown)
-    
     if org.description:
-        print(org.description, end="\n\n", file=markdown)
+        print(f"""## <img src="{org.avatarUrl}" width=24> [{org.name}]({org.url} "{org.description}")""", file=markdown)
+    else:
+        print(f"""## <img src="{org.avatarUrl}" width=24> [{org.name}]({org.url})""", file=markdown)
     
     if org.repositories.nodes:
         print("### Repositories", file=markdown)
         
         for repo in org.repositories.nodes:
             if repo.description:
-                print(f"- [{repo.name}]({repo.url}): {repo.description}", file=markdown)
+                print(f"""- [{repo.name}]({repo.url} "{repo.description}")""", file=markdown)
             else:
                 print(f"- [{repo.name}]({repo.url})", file=markdown)
 
